60a8ad894304a90f5e4e24a477e912dac5d43b39
Olivia <166169277+oliviaalmseger@users.noreply.github.com>
2025-06-05 15:03:23 +0200

    Första commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { CdAndCry } from './components/CdAndCry'
+
+function App() {
+
+
+  return (
+    <>
+      <CdAndCry />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/CdAndCry.tsx
@@ -0,0 +1,15 @@
+import './CdAndCry.css'; 
+
+export const CdAndCry = () => {
+    return (
+    <>
+    <div className="container">
+        <img src="./cdandcryolivia.jpg" alt="A black cap with the embroidered text 'cd ~ && cry'" />
+        <h1>cd ~ && cry</h1>
+        <p>— the original. Everything else is a fork 😉</p>
+
+
+    </div>
+    </>
+    )
+}
